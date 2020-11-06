be5050a911bfe58119d60720b3eb405a43b0fadc
John Newberry <70030285+Newber0@users.noreply.github.com>
2020-11-06 11:25:13 -0800

    Final update
    Should be finished here

--- Initial_structure_preprocessing.py
+++ Initial_structure_preprocessing.py
@@ -1,14 +1,20 @@
 import os
-import ants
-import numpy as np
-import pandas as pd
-import glob
-import shutil
-import random
-import nipype.interfaces.fsl as fsl
-import nipype.pipeline.engine as pe 
 
-# Data retrieved from ADNI Database http://adni.loni.usc.edu/ including csv file with identifying information for each file
-index = '/file.csv'
-df = pd.read_excel(index)
+## Renaming of Files
 
+filepath = '/Data_Directory/'
+# '/Data_Directory/' is wherever the data is stored.
+
+for f in os.listdir(filepath):
+  
+    # splitting filename into components
+    file_name, file_ext = os.path.splitext(f)
+    ADN, REDUNDANT, ID = file_name.split('I')
+    
+    # mild formatting to ensure names match up
+    ID = ID.strip()
+    file_ext = file_ext.strip()
+
+    # Renaming of files
+    new_name = ('{}{}'.format(ID,file_ext))
+    os.rename((filepath + f), (filepath + new_name)) 
