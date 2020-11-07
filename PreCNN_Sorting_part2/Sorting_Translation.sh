7468fbf137b8b803f5c0f8e84680a1499fb25df7
John Newberry <70030285+Newber0@users.noreply.github.com>
2020-11-06 21:10:57 -0800

    Update Sorting_Translation.sh

--- PreCNN_Sorting_part2/Sorting_Translation.sh
+++ PreCNN_Sorting_part2/Sorting_Translation.sh
@@ -1,9 +1,16 @@
+# Moving AD files
 cd /Output_ants_Translation/AD/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/ADvsCN/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/ADvsMCI/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/ADvsMCIvsCN/
+
+# Moving CN files
 cd /Output_ants_Translation/CN/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/ADvsCN/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/MCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/ADvsMCIvsCN/
+# Moving MCI files
 cd /Output_ants_Translation/MCI/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/ADvsMCI/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/MCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/ADvsMCIvsCN/
