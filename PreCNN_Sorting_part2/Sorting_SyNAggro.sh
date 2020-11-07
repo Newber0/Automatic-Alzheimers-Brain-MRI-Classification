83d913b4397d727e6e06fe1817b6a89f07b00a93
John Newberry <70030285+Newber0@users.noreply.github.com>
2020-11-06 21:09:19 -0800

    Update Sorting_SyNAggro.sh

--- PreCNN_Sorting_part2/Sorting_SyNAggro.sh
+++ PreCNN_Sorting_part2/Sorting_SyNAggro.sh
@@ -1,10 +1,17 @@
-  
+ # Moving AD files 
 cd /Output_ants_SyNAggro/AD/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNAggro/DataSamples/ADvsCN/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNAggro/DataSamples/ADvsMCI/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNAggro/DataSamples/ADvsMCIvsCN/
+
+# Moving CN files
 cd /Output_ants_SyNAggro/CN/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNAggro/DataSamples/ADvsCN/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNAggro/DataSamples/MCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNAggro/DataSamples/ADvsMCIvsCN/
+
+# Moving MCI files
 cd /Output_ants_SyNAggro/MCI/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNAggro/DataSamples/ADvsMCI/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNAggro/DataSamples/MCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNAggro/DataSamples/ADvsMCIvsCN/
