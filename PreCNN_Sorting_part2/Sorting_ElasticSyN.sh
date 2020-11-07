470e35b5424e2cd71eff7f768bb6abc71f27c8fe
John Newberry <70030285+Newber0@users.noreply.github.com>
2020-11-06 21:08:59 -0800

    Update Sorting_ElasticSyN.sh

--- PreCNN_Sorting_part2/Sorting_ElasticSyN.sh
+++ PreCNN_Sorting_part2/Sorting_ElasticSyN.sh
@@ -1,17 +1,17 @@
 # Moving AD files
 cd /Output_ants_ElasticSyN/AD/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/ADvsCN/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/ADvsMCI/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/ADvsMCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/Datasamples/ADvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/Datasamples/ADvsMCI/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/Datasamples/ADvsMCIvsCN/
 
 # Moving CN files
 cd /Output_ants_ElasticSyN/CN/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/ADvsCN/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/MCIvsCN/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/ADvsMCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/Datasamples/ADvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/Datasamples/MCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/Datasamples/ADvsMCIvsCN/
 
 # Moving MCI files
 cd /Output_ants_ElasticSyN/MCI/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/ADvsMCI/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/MCIvsCN/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/ADvsMCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/Datasamples/ADvsMCI/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/Datasamples/MCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_ElasticSyN/Datasamples/ADvsMCIvsCN/
