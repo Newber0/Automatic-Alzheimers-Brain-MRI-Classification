7e6f47400bb753ad803c22173398c49473a0731a
John Newberry <70030285+Newber0@users.noreply.github.com>
2020-11-06 21:11:53 -0800

    Update Sorting_Translation.sh

--- PreCNN_Sorting_part2/Sorting_Translation.sh
+++ PreCNN_Sorting_part2/Sorting_Translation.sh
@@ -1,16 +1,16 @@
 # Moving AD files
 cd /Output_ants_Translation/AD/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/ADvsCN/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/ADvsMCI/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/ADvsMCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/Datasamples/ADvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/Datasamples/ADvsMCI/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/Datasamples/ADvsMCIvsCN/
 
 # Moving CN files
 cd /Output_ants_Translation/CN/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/ADvsCN/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/MCIvsCN/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/ADvsMCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/Datasamples/ADvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/Datasamples/MCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/Datasamples/ADvsMCIvsCN/
 # Moving MCI files
 cd /Output_ants_Translation/MCI/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/ADvsMCI/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/MCIvsCN/
-find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/ADvsMCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/Datasamples/ADvsMCI/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/Datasamples/MCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_Translation/Datasamples/ADvsMCIvsCN/
