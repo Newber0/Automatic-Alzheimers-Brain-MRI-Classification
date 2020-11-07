fbfe47337a6b2e3032b5532c1d5de33b5b0c72ab
John Newberry <70030285+Newber0@users.noreply.github.com>
2020-11-06 21:10:08 -0800

    Update Sorting_SyNRA.sh

--- PreCNN_Sorting_part2/Sorting_SyNRA.sh
+++ PreCNN_Sorting_part2/Sorting_SyNRA.sh
@@ -1,10 +1,17 @@
-  
+# Moving AD files
 cd /Output_ants_SyNRA/AD/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNRA/DataSamples/ADvsCN/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNRA/DataSamples/ADvsMCI/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNRA/DataSamples/ADvsMCIvsCN/
+
+# Moving CN files
 cd /Output_ants_SyNRA/CN/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNRA/DataSamples/ADvsCN/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNRA/DataSamples/MCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNRA/DataSamples/ADvsMCIvsCN/
+
+# Moving MCI files
 cd /Output_ants_SyNRA/MCI/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNRA/DataSamples/ADvsMCI/
 find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNRA/DataSamples/MCIvsCN/
+find . -maxdepth 1 -type f | head -100 | xargs cp -t /Output_ants_SyNRA/DataSamples/ADvsMCIvsCN/
