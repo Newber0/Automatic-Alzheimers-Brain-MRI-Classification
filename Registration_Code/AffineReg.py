40c86da30fe9d769cd45cea85a67dbb3d9d5ff22
John Newberry <70030285+Newber0@users.noreply.github.com>
2020-11-06 19:38:36 -0800

    Update AffineReg.py

--- Registration_Code/AffineReg.py
+++ Registration_Code/AffineReg.py
@@ -16,11 +16,11 @@ for i in mylist:
   moving = ants.image_read(link+i)
   mytx = ants.registration(fixed=fixed, moving=moving, type_of_transform='Affine' )
   mywarpedimage = ants.apply_transforms(fixed=fixed, moving=moving, transformlist=mytx['fwdtransforms'])
-  mywarpedimage.to_file('/Output_ants_Affine/reg_ants/'+i)
+  mywarpedimage.to_file('/Output_ants_Affine/Registration/'+i)
   
   #segmentation
   mask=ants.get_mask(mywarpedimage)
   img_seg = ants.atropos(a=mywarpedimage, m='[0.2,1x1x1]', c='[2,0]', i='kmeans[3]', x=mask)
   gm =img_seg['probabilityimages'][1]
-  gm.to_file('/Output_ants_Affine/greymatter/'+i)
+  gm.to_file('/Output_ants_Affine/Greymatter/'+i)
   continue;
